e712fd85da1782d7f5043ece62753b3dbdcc26e3
SHIN1501020 <shin8hh@gmail.com>
2023-09-02 20:42:41 +0900

    소스 정리

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 import express from "express";
-import connect from "./schemas/index.js";
 import routes from "./routes/index.js"
-
-import { config } from "dotenv";
-config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-connect();
 // Express에서 req.body에 접근하여 body 데이터를 사용할 수 있도록 설정합니다.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
